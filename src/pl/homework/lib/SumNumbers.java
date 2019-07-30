1368af45c2dd13fbec0dd55a7691eb3d65aaaa20
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-30 10:26:10 +0200

    Homework 9.2 removed commented lines from the SumNumbers method

--- src/pl/homework/lib/SumNumbers.java
+++ src/pl/homework/lib/SumNumbers.java
@@ -4,11 +4,9 @@ public class SumNumbers {
 
     public static int sum(int number) {
         int sum = 0;
-//        int result = 0;
         int n = number;
         for(int i = 1; i < number; i *= 10) {
             sum += n%10;
-//            result = result*10 + n%10;
             n = n/10;
         }
         return sum;
